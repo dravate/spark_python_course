9f89c6bd0d7155a3d84efe7e6e69c08af59ea90f
Sopan Shewale <sopan.shewale@gmail.com>
2021-07-15 18:58:03 +0530

    task 6

---
+++ session_04/for_demo_adv.py
@@ -0,0 +1,18 @@
+
+food_items = [
+    "ham",
+    #"spam",
+    "eggs",
+    "nuts"
+]
+
+for food in food_items:
+    if food == "spam":
+        print ("No spam please")
+        break
+    print ("Great - ", food)
+#else:
+#   print ("I am glad - There was no spam")
+
+
+print ("Finally I have finished")
---
+++ session_04/task_6.py
@@ -0,0 +1,7 @@
+
+for i in range(20):
+    if i>9:
+        print( "x" * (20-i))
+    else:
+        print("x" * i)
+
