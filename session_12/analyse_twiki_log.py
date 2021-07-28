f2cf52c01de275b55d8b1d0c151f11d1e2bd1821
Sopan Shewale <sopan.shewale@gmail.com>
2021-07-28 20:35:04 +0530

    updated code

--- session_12/analyse_twiki_log.py
+++ session_12/analyse_twiki_log.py
@@ -1,18 +1,27 @@
 import sys
 import os
+
 # Read the File
+
 
 def get_lines_list (log_file):
     log_lines = []
-    with open (log_file, 'r') as f:
-        for line in f.readlines():
+    with open (log_file, 'r')  as mylog:
+        for line in mylog.readlines():
             log_lines.append(line)
+
     return log_lines
 
-def get_username( line ):
+def get_fields( line ):
     # line = '| 2017-01-01 - 07:54:57 | BenazirBhutto | edit | Fullgc.WebHome |  | 171.155.32.163 | '
-    line_array = line.split(' | ')
-    return line_array
+    line_array = line.split('|')
+    # Better ways, use regex, remove spaces
+    #TODO - use map
+    #new_array = []
+    #for element in line_array:
+    #    new_array.append(element.strip())
+    new_array = map(lambda element: element.strip(), line_array)
+    return list(new_array)
 
 def create_dictionary_user_activity(user_activity_lines):
     #print (user_activity_lines)
@@ -32,19 +41,31 @@ def create_dictionary_user_activity(user_activity_lines):
         print (line)
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print ("You need to provided logfile name as an argument")
+    #  python analyse_twiki_log.py TWiki_Application.log OprahWinfrey
+    # execute this script as above line
+    print(sys.argv)
+    print (len(sys.argv))
+    if len(sys.argv) != 3:
+        print ("check your arguments")
         sys.exit(0)
+
     log_file = sys.argv[1]
+    user = sys.argv[2]
+    print("You have passed logfile: {}".format(log_file))
     # check the presence of the file
     if os.path.exists(log_file):
         print ("Reading Log File: ", log_file)
     else:
         print ("The {} is not present in filesystem".format(log_file))
-        sys.exit(0)
+        sys.exit(1)
     # At this stage - the log file is present
+
+
     log_lines = get_lines_list(log_file)
-    #print (log_lines)
+    #for element in log_lines[0:4]:
+    #    print (element.strip())
+    #sys.exit(0)
+
     # I have read the log file, has lines in list
     '''
     | 2017-01-01 - 07:01:43 | JawaharlalNehru | view | Main.WebHome | Mozilla | 253.111.182.73 | 
@@ -58,23 +79,48 @@ if __name__ == '__main__':
     5. How about finding busiest Day? 
     
     '''
-    for line in log_lines:
-        line_fields =  get_username(line)
-        #print (line_fields[1])
+    #for line in log_lines[0:4]:
+    #    line_fields =  get_fields(line)
+    #    #print (line_fields)
+    #sys.exit(0)
+
+
 
     unique_users = {}
     for line in log_lines:
-        line_fields = get_username(line)
-        unique_users[line_fields[1]] = 1
+        line_fields = get_fields(line)
+        #unique_users[line_fields[2]] = [line_fields[1]]
+        if line_fields[2] in unique_users:
+            unique_users[line_fields[2]].append([
+                 line_fields[1],
+                 line_fields[3],
+                 line_fields[4],
+                 line_fields[5]
+                 ])
+        else:
+            unique_users[line_fields[2]] = [[
+                line_fields[1],
+                line_fields[3],
+                line_fields[4],
+                line_fields[5]
+            ]]
 
+
+    print("\n")
+    #for key in unique_users:
+    #    print (key, unique_users[key][0:4])
+    #
+    for activty in unique_users[user]:
+        print (activty)
+    sys.exit(0)
+
+    # How many unique uers?
     #print (unique_users)
-    # How many unique users?
-    print (len(unique_users))
-
+    #sys.exit(0)
     # Is BillGates Present in the log
-    if 'BillGatesHari' in unique_users:
+    if 'BillGates' in unique_users:
         print ("Bill Gates is Present")
     else:
         print ("Bill Gates does not exists in the log file")
-
+    sys.exit(0)
     user_activity_details = create_dictionary_user_activity(log_lines)
